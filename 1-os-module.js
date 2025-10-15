02f609b132f6e9557777a6201162b0ecaf3b521e
Divyahub7 <divya14042006@gmail.com>
2025-10-15 23:40:01 +0530

    Organized the files

---
+++ 1-os-module.js
@@ -0,0 +1,18 @@
+// os module in node j
+
+const os = require("os");
+
+//Info about user
+const user = os.userInfo();
+console.log(user);
+
+// method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+console.log(currentOS);
--- secondmodules.js
+++ secondmodules.js
@@ -1,5 +1,5 @@
 const names = require("./firstmodule");
-const sayHii = require("./app");
+const sayHii = require("./utils");
 sayHii(names.divya);
 sayHii(names.lisa);
 sayHii(names.cookie);
